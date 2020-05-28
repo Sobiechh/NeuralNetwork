3afc7a5c40a22b093bbfd2dd22a56850cf0f13ee
Piotr Sobieszczyk <p.sobieszczyk1@gmail.com>
2020-05-29 01:45:58 +0200

    forward prop

--- nerual.py
+++ nerual.py
@@ -1,15 +1,59 @@
 import numpy as np
 import matplotlib
 
-inputs = [1, 2, 3, 2.5]
+np.random.seed(0)
 
-weights = [
-    [0.2, 0.8, -0.5, 1],
-    [0.5, -0.91, 0.26, -0.5],
-    [-0.26, -0.27, 0.17, 0.87]
+X = [
+    [1.0, 2.0, 3.0, 2.5],
+    [2.0, 5.0, -1.0, 2.0],
+    [-1.5, 2.7, 3.3, -0.8]
 ]
 
-biases = [2,3,0.5]
+X2 = [
+    [1.1, -1.0, 3.0],
+    [1.2, 8.9, -1.0],
+    [-7.5, 2.3, 1.3],
+    [1.5, -3.5, 2.4]
+]
 
-output = np.dot(weights, inputs) + biases #we want 3 neurons, np.dot(inputs, weights) won't work, VECTORS
-print(output)
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons)) #tuple
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+'''
+np.random.randn(3, 4)
+[[ 1.76405235  0.40015721  0.97873798  2.2408932 ]
+ [ 1.86755799 -0.97727788  0.95008842 -0.15135721]
+ [-0.10321885  0.4105985   0.14404357  1.45427351]]
+'''
+
+''' ex1
+layer1 = Layer_Dense(4, 5) #(how many features, numb of neurons)
+layer2 = Layer_Dense(5, 2) # 5==5
+
+layer1.forward(X)
+#print(layer1.output)
+
+layer2.forward(layer1.output)
+print(layer2.output)
+'''
+
+
+'''
+X2 = [
+    [1.1, -1.0, 3.0],
+    [1.2, 8.9, -1.0],
+    [-7.5, 2.3, 1.3],
+    [1.5, -3.5, 2.4]
+]
+'''
+layer1 = Layer_Dense(3, 10) #3 couse x2 colmumns(features)
+layer2 = Layer_Dense(10, 2) 
+
+layer1.forward(X2)
+
+layer2.forward(layer1.output)
+print(layer2.output)
--- temp.py
+++ temp.py
@@ -8,7 +8,7 @@ bias = 2
 #     print('same shapes')
 
 output = np.dot(weights, inputs) + bias # np.dot(weights, inputs) doesn't matter here because all shapes(4,)
-print(output)
+#print(output)
 
 '''
 layer_outputs = []
@@ -21,3 +21,54 @@ for neuron_weights, neuron_bias in zip(weights, biases):
 
 print(layer_outputs)
 '''
+
+
+tab1 = [
+    [1,2,3],
+    [4,5,6]
+]
+
+print(np.shape(tab1))
+
+tab2 = [
+    [2,1],
+    [8,8],
+    [5,6]
+]
+
+wynik = np.dot(tab2, tab1)
+
+print(wynik)
+
+inputs = [
+    [1.0, 2.0, 3.0, 2.5],
+    [2.0, 5.0, -1.0, 2.0],
+    [-1.5, 2.7, 3.3, -0.8]
+]
+
+weights = [
+    [0.2, 0.8, -0.5, 1.0],
+    [0.5, -0.91, 0.26, -0.5],
+    [-0.26, -0.27, 0.17, 0.87]
+]
+
+biases = [2,3,0.5]
+
+weights2 = [
+    [0.1, -0.14, 0.5],
+    [-0.5, 0.12, -0.33],
+    [-0.44, 0.73, -0.13]
+]
+
+biases2 = [-1.0, 2.0, -0.5]
+
+layer1_outputs = np.dot(inputs, np.array(weights).T) + biases #we have to transpose weights0
+#biases dont have to be shape(3,3)
+#layer1_outputs
+# [[ 4.8    1.21   2.385]
+#  [ 8.9   -1.81   0.2  ]
+#  [ 1.41   1.051  0.026]]
+
+layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
+
+print(layer2_outputs)
